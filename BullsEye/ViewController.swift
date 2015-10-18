e459e7a417a76225d36e3ec5320bbda5e509b9c2
Patrick Schneider <me@patrickschneider.io>
2015-10-18 21:52:02 +0200

    Done game w/o polishing
    Aprrentice getting startet page 86

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var roundLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  var currentValue = 0
+  var targetValue = 0
+  var score = 0
+  var round = 0
+
+  func startNewRound() {
+    round += 1
+    currentValue = 50
+    targetValue = 1 + Int(arc4random_uniform(100))
+    slider.value = Float(currentValue)
+
+  }
+
+  func updateLabels() {
+    targetLabel.text = "\(targetValue)"
+    scoreLabel.text = "\(score)"
+    roundLabel.text = "\(round)"
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    startNewRound()
+    updateLabels()
+  }
+
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func showAlert() {
+    let difference = abs(currentValue - targetValue)
+    let points = 100 - difference
+    score += points
+
+    let message = "You scored \(points) points"
+    let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
+    let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+
+    alert.addAction(action)
+    presentViewController(alert, animated: true, completion: nil)
+
+    startNewRound()
+    updateLabels()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderMoved(sender: UISlider) {
+      currentValue = lroundf(sender.value)
     }
-
 
 }
 
