c40d630f2d004a8a354e34da216a99a8871549f6
Patrick Schneider <me@patrickschneider.io>
2015-10-20 13:12:33 +0200

    About screen + some polishing with new score / new game

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Patrick Schneider on 20/10/15.
+//  Copyright © 2015 Patrick Schneider. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+  @IBAction func close() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -19,6 +19,55 @@ class ViewController: UIViewController {
   var score = 0
   var round = 0
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    startNewGame()
+    updateLabels()
+  }
+
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func showAlert() {
+    let difference = abs(currentValue - targetValue)
+    var points = 100 - difference
+
+    var title: String
+    switch difference {
+    case _ where difference == 0:
+      title = "Perfect!"
+      points += 100
+    case let difference where difference < 5:
+      title = "You almost had it!"
+      if difference == 1 { points += 50 }
+    case _ where difference < 10:
+      title = "Pretty good!"
+    default:
+      title = "Not even close...!"
+    }
+    score += points
+    let message = "You scored \(points) points"
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+    let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+      self.startNewRound()
+      self.updateLabels()
+    })
+
+    alert.addAction(action)
+    presentViewController(alert, animated: true, completion: nil)
+  }
+
+  @IBAction func sliderMoved(sender: UISlider) {
+    currentValue = lroundf(sender.value)
+  }
+
+  @IBAction func startOver() {
+    startNewGame()
+    updateLabels()
+  }
+
   func startNewRound() {
     round += 1
     currentValue = 50
@@ -33,36 +82,11 @@ class ViewController: UIViewController {
     roundLabel.text = "\(round)"
   }
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  func startNewGame() {
+    round = 0
+    score = 0
     startNewRound()
-    updateLabels()
   }
-
-  override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func showAlert() {
-    let difference = abs(currentValue - targetValue)
-    let points = 100 - difference
-    score += points
-
-    let message = "You scored \(points) points"
-    let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
-    let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
-
-    alert.addAction(action)
-    presentViewController(alert, animated: true, completion: nil)
-
-    startNewRound()
-    updateLabels()
-    }
-
-    @IBAction func sliderMoved(sender: UISlider) {
-      currentValue = lroundf(sender.value)
-    }
 
 }
 
